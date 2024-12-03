89620a6a5afa25a28598228de683a1f811d7a0d1
Umputun <umputun@gmail.com>
2024-12-03 12:33:59 -0600

    refactor the app to be more idiomatic

--- main.go
+++ main.go
@@ -1,123 +1,180 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-pkgz/lgr"
 	"github.com/umputun/go-flags"
 )
 
-type Options struct {
-	Check         bool          `long:"check" env:"CHECK" description:"Enable status check"`
+var opts struct {
 	CheckURL      string        `long:"check-url" env:"CHECK_URL" default:"http://icecast:8000/status-json.xsl" description:"URL to check the stream status"`
-	CheckInterval time.Duration `long:"check-interval" env:"CHECK_INTERVAL" default:"60s" description:"Interval for status checks (in seconds)"`
-	StreamURL     string        `long:"stream-url" env:"STREAM_URL" default:"https://stream.radio-t.com" description:"Source stream URL"`
-	TGServer      string        `long:"tg-server" env:"TG_SERVER" default:"dc4-1.rtmp.t.me" description:"Telegram server"`
-	TGKey         string        `long:"tg-key" env:"TG_KEY" required:"true" description:"Telegram stream key"`
-	Debug         bool          `long:"debug" env:"DEBUG" description:"Enable debug mode"`
+	CheckInterval time.Duration `long:"check-interval" env:"CHECK_INTERVAL" default:"60s" description:"Interval for status checks"`
+	CheckTimeout  time.Duration `long:"check-timeout" env:"CHECK_TIMEOUT" default:"5s" description:"Timeout for status check"`
+
+	StreamURL  string `long:"stream-url" env:"STREAM_URL" default:"https://stream.radio-t.com" description:"Source stream URL"`
+	FfmpegPath string `long:"ffmpeg-path" env:"FFMPEG_PATH" default:"/usr/bin/ffmpeg" description:"Path to ffmpeg binary"`
+	SkipCheck  bool   `long:"skip-check" env:"SKIP_CHECK" description:"Disable status check"`
+	TGServer   string `long:"tg-server" env:"TG_SERVER" default:"dc4-1.rtmp.t.me" description:"Telegram server"`
+	TGKey      string `long:"tg-key" env:"TG_KEY" required:"true" description:"Telegram stream key"`
+	Debug      bool   `long:"debug" env:"DEBUG" description:"Enable debug mode"`
 }
 
-var (
-	opts Options
-	log  = lgr.New(lgr.Msec)
-)
+var revision = "unknown"
 
-func checkStatus() {
-	status := false
-	for !status {
-		log.Logf("[DEBUG] Checking: %s", opts.CheckURL)
-		resp, err := http.Get(opts.CheckURL)
-		if err != nil {
-			log.Logf("[DEBUG] FAIL: %v", err)
-			time.Sleep(opts.CheckInterval)
-			continue
+func main() {
+	fmt.Printf("tg-retrans, %s\n", revision)
+	if _, err := flags.Parse(&opts); err != nil {
+		log.Printf("[ERROR] failed to parse flags: %v", err)
+		os.Exit(2)
+	}
+	setupLog(opts.Debug)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := run(ctx); err != nil {
+		log.Printf("[ERROR] run failed: %v", err)
+		os.Exit(1)
+	}
+	log.Printf("[INFO] completed")
+}
+
+// run is the main function that starts the retranslation process
+// it is one-shot if SkipCheck is set, otherwise it runs in a loop with a check interval
+func run(ctx context.Context) error {
+	if opts.SkipCheck {
+		if err := startRetrans(ctx); err != nil {
+			return fmt.Errorf("failed to start retranslation: %w", err)
 		}
-		defer resp.Body.Close()
+		if !checkStreamStatus(ctx) {
+			return fmt.Errorf("stream is not available")
+		}
+		return nil
+	}
 
-		var data map[string]interface{}
-		if err := json.NewDecoder(resp.Body).Decode(&data); err == nil {
-			if icestats, ok := data["icestats"].(map[string]interface{}); ok {
-				if sources, ok := icestats["source"]; ok && sources != nil {
-					log.Logf("[DEBUG] SUCCESS")
-					status = true
-					continue
-				}
+	for {
+		// cancel the context if the parent is done
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		if checkStreamStatus(ctx) {
+			log.Print("[DEBUG] Stream is available, start retranslation")
+			if err := startRetrans(ctx); err != nil {
+				log.Printf("[WARN] failed to start retranslation: %v", err)
 			}
+		} else {
+			log.Printf("[DEBUG] Not streaming, next check in %v", opts.CheckInterval)
 		}
-
-		log.Logf("[DEBUG] FAIL: invalid data")
-		log.Logf("[DEBUG] Next check in %s", opts.CheckInterval)
 		time.Sleep(opts.CheckInterval)
 	}
 }
 
-func startWork() {
-	destURL := fmt.Sprintf("rtmps://%s/s/%s", opts.TGServer, opts.TGKey)
+// checkStreamStatus checks if the stream is available
+func checkStreamStatus(ctx context.Context) bool {
+	log.Printf("[DEBUG] Checking stream with %s", opts.CheckURL)
+	client := http.Client{Timeout: opts.CheckTimeout}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, opts.CheckURL, http.NoBody)
+	if err != nil {
+		log.Printf("[DEBUG] Can't make request to %s: %v", opts.CheckURL, err)
+		return false
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("[DEBUG] Can't get response from %s: %v", opts.CheckURL, err)
+		return false
+	}
+	defer resp.Body.Close()
 
-	log.Logf("[INFO] Start retranslation")
-	log.Logf("[INFO] Source: %s", opts.StreamURL)
-	log.Logf("[INFO] Destination: %s", destURL)
-
-	runOpts := []string{
-		"-v", "verbose",
-		"-nostdin",
-		"-nostats",
-		"-hide_banner",
-		"-loop", "1",
-		"-i", "logo-dark.png",
-		"-i", opts.StreamURL,
-		"-c:v", "libx264",
-		"-tune", "stillimage",
-		"-pix_fmt", "yuv420p",
-		"-c:a", "aac",
-		"-b:a", "128k",
-		"-ac", "1",
-		"-ar", "44100",
-		"-f", "flv",
-		"-rtmp_live", "-1",
-		destURL,
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("[DEBUG] Invalid status code for %s: %d", opts.CheckURL, resp.StatusCode)
+		return false
 	}
 
-	log.Logf("[DEBUG] Run options: %v", runOpts)
-
-	cmd := exec.Command("/usr/bin/ffmpeg", runOpts...)
-	if opts.Debug {
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stdout
-	} else {
-		cmd.Stdout = nil
-		cmd.Stderr = nil
+	data := map[string]any{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		log.Printf("[DEBUG] Failed to decode response: %v", err)
+		return false
 	}
 
-	if err := cmd.Run(); err != nil {
-		log.Logf("[ERROR] Failed to run ffmpeg: %v", err)
+	icestats, ok := data["icestats"].(map[string]any)
+	if !ok {
+		log.Printf("[DEBUG] Missing icestats key in response")
+		return false
 	}
 
-	log.Logf("[INFO] End retranslation")
+	if sources, ok := icestats["source"]; !ok || sources == nil {
+		log.Printf("[DEBUG] FAIL: missing or empty source")
+		return false
+	}
+
+	log.Printf("[DEBUG] SUCCESS")
+	return true
 }
 
-func main() {
-	parser := flags.NewParser(&opts, flags.Default)
-	if _, err := parser.Parse(); err != nil {
-		log.Logf("[ERROR] Can't read params")
-		os.Exit(1)
-	}
-
-	if opts.Debug {
-		log = lgr.New(lgr.Msec, lgr.Debug, lgr.CallerFunc)
-		log.Logf("[DEBUG] Debug mode enabled")
-		log.Logf("Options: %+v\n", opts)
-	}
-
-	for {
-		if opts.Check {
-			checkStatus()
+func startRetrans(ctx context.Context) error {
+	// spawnFFmpeg creates and runs ffmpeg process
+	spawnFFmpeg := func(destURL string) error {
+		runOpts := []string{
+			"-v", "verbose",
+			"-nostdin",
+			"-nostats",
+			"-hide_banner",
+			"-loop", "1",
+			"-i", "logo-dark.png",
+			"-i", opts.StreamURL,
+			"-c:v", "libx264",
+			"-tune", "stillimage",
+			"-pix_fmt", "yuv420p",
+			"-c:a", "aac",
+			"-b:a", "128k",
+			"-ac", "1",
+			"-ar", "44100",
+			"-f", "flv",
+			"-rtmp_live", "-1",
+			destURL,
 		}
-		startWork()
+
+		log.Printf("[DEBUG] Run options: %v", runOpts)
+		cmd := exec.CommandContext(ctx, opts.FfmpegPath, runOpts...)
+
+		if opts.Debug {
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stdout
+		}
+
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to run ffmpeg: %w", err)
+		}
+		return nil
 	}
+
+	destURL := fmt.Sprintf("rtmps://%s/s/%s", opts.TGServer, opts.TGKey)
+	log.Printf("[INFO] Start retranslation from %s to %s", opts.StreamURL, destURL)
+	if err := spawnFFmpeg(destURL); err != nil {
+		return fmt.Errorf("failed to start retranslation: %w", err)
+	}
+
+	log.Printf("[INFO] End retranslation")
+	return nil
+}
+
+func setupLog(dbg bool) {
+	logOpts := []lgr.Option{lgr.Msec, lgr.LevelBraces, lgr.StackTraceOnError}
+	if dbg {
+		logOpts = []lgr.Option{lgr.Debug, lgr.CallerFile, lgr.CallerFunc, lgr.Msec, lgr.LevelBraces, lgr.StackTraceOnError}
+	}
+	lgr.SetupStdLogger(logOpts...)
 }
